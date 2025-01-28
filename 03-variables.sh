02938bbc42e4a5f2fea93274c5377b4c4edae249
nikitha84 <nikitha84640@gmail.com>
2025-01-28 15:22:43 +0530

    new

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1" : "Hi $PERSON2"
+echo "$PERSON2" : "Hello $PERSON1"
+echo "$PERSON1" : "Good mrng $PERSON2"
+echo "$PERSON2" : "Good mrng $PERSON1"
