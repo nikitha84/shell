d1f12cb9b3902e7b6bc2f64979beddefd8201824
nikitha84 <nikitha84640@gmail.com>
2025-01-28 16:26:27 +0530

    variables

--- 04-arrays.sh
+++ 04-arrays.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-FRUITS= ("apply" "banana" "mango")
+FRUITS=("apply" "banana" "mango")
 
-echo "first value: ${FRUITS[0]}"
+echo "first value: $(FRUITS[0])"
 echo "first value: ${FRUITS[1]}"
 echo "first value: ${FRUITS[2]}"
