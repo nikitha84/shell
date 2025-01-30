5f3f9cebd2f82d7de3629527e2f5f8a7475e7d24
nikitha84 <nikitha84640@gmail.com>
2025-01-30 16:30:56 +0530

    conditions

---
+++ 07-functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then 
+        echo "ERROR: Git installation failed"
+    else
+        echo "Git installation Success"
+    fi
+}
+
+ID=$(id -u)
+if [ $ID -ne 0 ]
+then 
+    echo "ERROR : Please run with root user"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+yum install git -y
+
+VALIDATE
+yum install git -y
+VALIDATE
+
