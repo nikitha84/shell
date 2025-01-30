0d3f6e2d349ebb807e33ce50d70fbfcae251826c
nikitha84 <nikitha84640@gmail.com>
2025-01-30 15:35:19 +0530

    conditions

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ID =$(id -u)
+ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then
-    echo "ERROR: Please run root user"
+    echo "ERROR: Please run with root user"
 else
     echo "You are root user"
 fi
