5027c2211fba42f4e03c91b0625a3da7eed8e24a
nikitha84 <nikitha84640@gmail.com>
2025-01-30 15:57:53 +0530

    conditions

---
+++ 06-inatall-git.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Installing git & validate conditons
+
+ID=$(id -u)
+if [ $ID -ne 0 ]
+then 
+    echo "ERROR : Please run with root user"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+yum install git -y
+if [ $? -ne 0 ]
+then 
+    echo "ERROR: Git installation failed"
+else
+    echo " Git installation Success
+fi    
