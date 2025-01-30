b577ffa4a07211e8ae89134d515d8371f2f4ab42
nikitha84 <nikitha84640@gmail.com>
2025-01-30 15:45:51 +0530

    conditions

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -5,9 +5,14 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "ERROR: Please run with root user"
+    exit 1 #$? to see exit status
 else
     echo "You are root user"
 fi
+
+yum install mysql -y
+
+
 
 
 
